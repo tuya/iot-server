22d87f3f4b4bc1ff5f7a03c1c9c145d42c67c215
zheye <zheye.zhang@tuya.com>
2021-05-19 15:42:43 +0800

    feat: reset password

---
+++ iot-suite-server-ability/src/main/java/com/tuya/iot/suite/ability/notice/model/ResetPasswordReq.java
@@ -0,0 +1,24 @@
+package com.tuya.iot.suite.ability.notice.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * <p> TODO
+ *
+ * @author 哲也
+ * @since 2021/5/19
+ */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordReq {
+
+    String username;
+
+    String new_password;
+}
