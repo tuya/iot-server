adff007dab3b361dc9566b26ddcea3bdd302d6db
zhoujiaping <421211679@qq.com>
2021-05-31 16:07:38 +0800

    reactor: 重构登陆/注销功能，使用shiro方式登陆/注销

--- iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/config/shiro/TuyaCloudRealm.java
+++ iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/config/shiro/TuyaCloudRealm.java
@@ -1,7 +1,10 @@
 package com.tuya.iot.suite.web.config.shiro;
 
+import com.tuya.iot.suite.core.model.UserToken;
+import com.tuya.iot.suite.service.user.UserService;
+import com.tuya.iot.suite.web.config.HttpRequestUtils;
+import lombok.Setter;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AccountException;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -11,6 +14,7 @@ import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,7 +23,9 @@ import java.util.Set;
  * @author: benguan.zhou@tuya.com
  * @date: 2021/05/28
  */
+@Setter
 public class TuyaCloudRealm extends AuthorizingRealm {
+    private UserService userService;
     /**
      * TODO
      */
@@ -43,16 +49,13 @@ public class TuyaCloudRealm extends AuthorizingRealm {
      */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        System.out.println("-------身份认证方法--------");
-        String userName = (String) authenticationToken.getPrincipal();
-        String userPwd = new String((char[]) authenticationToken.getCredentials());
-        //根据用户名从数据库获取密码
-        String password = "123";
-        if (userName == null) {
-            throw new AccountException("用户名不正确");
-        } else if (!userPwd.equals(password )) {
-            throw new AccountException("密码不正确");
-        }
-        return new SimpleAuthenticationInfo(userName, password,getName());
+        String username = (String) authenticationToken.getPrincipal();
+        String password = new String((char[]) authenticationToken.getCredentials());
+        String uid = userService.login(username,password).getUid();
+        HttpSession session = HttpRequestUtils.getHttpSession();
+        //小心，有两个类叫UserToken，在不同包下面，不要搞混了
+        UserToken userToken = new com.tuya.iot.suite.core.model.UserToken(uid, username, session.getId(), 1);
+        session.setAttribute("token", userToken);
+        return new SimpleAuthenticationInfo(username, password,getName());
     }
 }
