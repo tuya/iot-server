b0a872b63b53fd845d6de84e939df0d19391eb9c
mickey <miqi.yin@tuya.com>
2021-06-01 15:55:46 +0800

    feat：add role template

--- iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/controller/PermissionTemplateController.java
+++ iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/controller/PermissionTemplateController.java
@@ -6,11 +6,7 @@ import com.tuya.iot.suite.web.model.PermissionTemplate;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,20 +16,16 @@ import java.util.List;
  * @date 2021/05/28
  */
 @RestController
-@RequestMapping("/permission-templates")
+
 @Slf4j
 @Api(value = "权限模板")
 public class PermissionTemplateController {
 
-    /**
-     * 是否需要权限模板？
-     * 可以创建几个角色，当作权限模板。
-     * 创建其他角色，可以选择另一个角色当作源，创建其副本。
-     * 这样不仅可以实现需求，而且更灵活。管控台也不需要保存模板文件了。
-     * */
+
     @ApiOperation("查询权限模版列表")
+    @RequestMapping("/permission-template/role")
     @GetMapping
-    public Response<List<PermissionTemplate>> listPermTemplates(){
+    public Response<List<PermissionTemplate>> listPermTemplates(@RequestParam  String roleCode){
         return Todo.todo();
     }
 
--- iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/controller/RoleController.java
+++ iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/controller/RoleController.java
@@ -1,39 +1,20 @@
 package com.tuya.iot.suite.web.controller;
 
-import com.tuya.iot.suite.ability.idaas.model.IdaasRole;
-import com.tuya.iot.suite.ability.idaas.model.IdaasRoleCreateReq;
-import com.tuya.iot.suite.ability.idaas.model.RoleUpdateReq;
-import com.tuya.iot.suite.ability.idaas.model.RolesPaginationQueryReq;
-import com.tuya.iot.suite.ability.idaas.model.SuiteRoleCode;
 import com.tuya.iot.suite.core.constant.Response;
 import com.tuya.iot.suite.core.util.Todo;
-import com.tuya.iot.suite.service.idaas.RoleService;
 import com.tuya.iot.suite.service.model.PageVO;
-import com.tuya.iot.suite.service.model.RoleTypeEnum;
-import com.tuya.iot.suite.web.config.ProjectProperties;
-import com.tuya.iot.suite.web.model.RoleCreateReq;
-import com.tuya.iot.suite.web.model.RoleNameUpdateReq;
-import com.tuya.iot.suite.web.model.RoleVO;
-import com.tuya.iot.suite.web.util.SessionContext;
+import com.tuya.iot.suite.web.model.request.role.RoleAddReq;
+import com.tuya.iot.suite.web.model.request.role.RoleEditReq;
+import com.tuya.iot.suite.web.model.request.role.RolePermissionReq;
+import com.tuya.iot.suite.web.model.response.permission.PermissionDto;
+import com.tuya.iot.suite.web.model.response.role.RoleDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 
 /**
@@ -43,78 +24,45 @@ import java.util.stream.Collectors;
 @RestController
 @Slf4j
 @Api(value = "角色管理")
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RoleController {
 
-    @Autowired
-    RoleService roleService;
-    @Autowired
-    ProjectProperties projectProperties;
-
-    @ApiOperation("创建角色")
-    @PutMapping("/roles")
-    public Response<Boolean> createRole(@RequestBody RoleCreateReq req) {
-        String uid = SessionContext.getUserToken().getUserId();
-        //TODO 校验角色类型，不能创建超级管理员类型的角色
-        Boolean res = roleService.createRole(projectProperties.getSpaceId(), IdaasRoleCreateReq.builder()
-                .roleCode(
-                        SuiteRoleCode.builder()
-                                .code(SuiteRoleCode.randomCode())
-                                .type(req.getRoleType())
-                                .build()
-                                .toCloudRoleCode()
-                )
-                .roleName(req.getRoleName())
-                .build());
-        return Response.buildSuccess(res);
-    }
 
     @ApiOperation("角色列表")
     @GetMapping("/roles")
-    public Response<PageVO<RoleVO>> listRoles(Integer pageNo,Integer pageSize,String roleCode,String roleName) {
-        PageVO<IdaasRole> pageVO = roleService.queryRolesPagination(projectProperties.getSpaceId(),
-                RolesPaginationQueryReq.builder()
-                        .pageNum(pageNo)
-                        .pageSize(pageSize)
-                        .roleCode(roleCode)
-                        .roleName(roleName)
-                        .build());
-        return Response.buildSuccess(PageVO.builder().pageNo(pageVO.getPageNo())
-                .pageSize(pageVO.getPageSize())
-                .total(pageVO.getTotal())
-                .data((List)pageVO.getData().stream().map(
-                        it->
-                                RoleVO.builder().typeCode(RoleTypeEnum.fromRoleCode(it.getRoleCode()).name())
-                        .code(it.getRoleCode())
-                        .name(it.getRoleName())
-                        .build()
-                ).collect(Collectors.toList())).build());
+    public Response<PageVO<RoleDto>> lislistRolestRoles() {
+        return Todo.todo();
     }
 
-    @ApiOperation("修改角色名称")
-    @PutMapping("/roles/{roleCode}/name")
-    public Response<Boolean> updateRoleName(@PathVariable String roleCode, @RequestBody RoleNameUpdateReq req) {
-        Boolean res = roleService.updateRole(projectProperties.getSpaceId(), roleCode,
-                RoleUpdateReq.builder().roleName(req.getName()).build());
-        return Response.buildSuccess(res);
+    @ApiOperation("创建角色")
+    @PostMapping("/role")
+    public Response<Boolean> createRole(@RequestBody RoleAddReq req) {
+        return Todo.todo();
     }
 
-    @ApiOperation("批量删除角色")
-    @DeleteMapping("/batch-roles")
-    public Response<Boolean> batchDeleteRole(@ApiParam(value = "角色编码列表，逗号分隔", required = true)
-                                             @RequestParam("roleCodeList") String roleCodeList) {
-        Set<String> roleCodes = StringUtils.commaDelimitedListToSet(roleCodeList);
-        boolean success = true;
-        for (String roleCode : roleCodes) {
-            success &= roleService.deleteRole(projectProperties.getSpaceId(),roleCode);
-        }
-        return Response.buildSuccess(success);
+    @ApiOperation("修改角色")
+    @PutMapping("/role")
+    public Response<Boolean> updateRoleName(@RequestBody RoleEditReq req) {
+        return Todo.todo();
     }
+
 
     @ApiOperation("删除角色")
-    @DeleteMapping("/roles/{roleCode}")
+    @DeleteMapping("/role/{roleCode}")
     public Response<Boolean> deleteRole(@PathVariable String roleCode) {
-        Boolean success = roleService.deleteRole(projectProperties.getSpaceId(),roleCode);
-        return Response.buildSuccess(success);
+        return Todo.todo();
+    }
+
+    @ApiOperation("给角色授权")
+    @PutMapping("/role/permissions")
+    @RequiresPermissions("roles")
+    public Response<Boolean> rolePermissions(@RequestBody RolePermissionReq req) {
+        return Todo.todo();
+    }
+
+    @ApiOperation("查角色拥有的授权")
+    @GetMapping("/role/permissions")
+    @RequiresPermissions("roles")
+    public Response<List<PermissionDto>> getRolePermissions(@RequestParam String roleCode) {
+        return Todo.todo();
     }
 }
