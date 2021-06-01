6ec8eb1c0f8da3560dbfb33feabefa3c56540262
mickey <miqi.yin@tuya.com>
2021-06-01 15:55:46 +0800

    feat：add role template

---
+++ iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/model/response/role/RoleDto.java
@@ -0,0 +1,34 @@
+package com.tuya.iot.suite.web.model.response.role;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * @author mickey
+ * @date 2021年06月01日 14:57
+ */
+@Getter
+@Setter
+@ToString
+public class RoleDto implements Serializable {
+    /**
+     *
+     */
+    @ApiModelProperty("角色code")
+    private String roleCode;
+    /**
+     *
+     */
+    @ApiModelProperty("角色名称")
+    private String roleName;
+    /**
+     *
+     */
+    @ApiModelProperty("角色备注")
+    private String remark;
+
+}
