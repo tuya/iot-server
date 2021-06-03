242157f8ee758c8f4bc107a69a69b6dc748ffb31
zhoujiaping <421211679@qq.com>
2021-06-03 20:40:20 +0800

    docs: 新增测试用例，两个创建角色失败用例

--- iot-suite-server-web/src/test/groovy/com/tuya/iot/suite/service/RoleServiceSpec.groovy
+++ iot-suite-server-web/src/test/groovy/com/tuya/iot/suite/service/RoleServiceSpec.groovy
@@ -3,6 +3,7 @@ package com.tuya.iot.suite.service
 import com.tuya.iot.suite.ability.idaas.ability.GrantAbility
 import com.tuya.iot.suite.ability.idaas.ability.RoleAbility
 import com.tuya.iot.suite.ability.idaas.model.IdaasRole
+import com.tuya.iot.suite.core.exception.ServiceLogicException
 import com.tuya.iot.suite.service.dto.RoleCreateReqDTO
 import com.tuya.iot.suite.service.idaas.RoleService
 import com.tuya.iot.suite.service.model.RoleCodeGenerator
@@ -21,7 +22,7 @@ class RoleServiceSpec extends BaseSpec {
     @Autowired
     RoleService roleService
 
-    void "测试创建角色-系统管理员"() {
+    void "测试创建角色-创建成功"() {
         given:
         def roleAbility = Mock(RoleAbility)
         def grantAbility = Mock(GrantAbility)
@@ -48,10 +49,76 @@ class RoleServiceSpec extends BaseSpec {
         when:
         def createResult = roleService.createRole(spaceId, RoleCreateReqDTO.builder()
                 .roleCode(RoleCodeGenerator.generate(RoleTypeEnum.manage,"1000"))
-                .roleName('admin1000')
+                .roleName('manage1000')
                 .build())
         then:
         createResult
     }
 
+
+    void "测试创建角色-创建失败-没有权限"() {
+        given:
+        def roleAbility = Mock(RoleAbility)
+        def grantAbility = Mock(GrantAbility)
+        roleService.roleAbility = roleAbility
+        roleService.grantAbility = grantAbility
+        roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
+                                                       .roleName('normal')
+                                                       .roleCode("normal-1000")
+                                                       .build()
+        ]
+        //roleAbility.createRole(_, _) >> true
+        roleAbility.createRole(_, _) >> {
+            spaceId, req ->
+                log.info("spaceId=$spaceId,req=$req")
+                true
+        }
+        grantAbility.grantPermissionsToRole(_) >> {
+            req ->
+                log.info("grant===>{}",req)
+                true
+        }
+
+        def spaceId = 1000
+        when:
+        def createResult = roleService.createRole(spaceId, RoleCreateReqDTO.builder()
+                .roleCode(RoleCodeGenerator.generate(RoleTypeEnum.manage,"1000"))
+                .roleName('manage1000')
+                .build())
+        then:
+        thrown(ServiceLogicException)
+    }
+
+    void "测试创建角色-创建失败-不能创建管理员"() {
+        given:
+        def roleAbility = Mock(RoleAbility)
+        def grantAbility = Mock(GrantAbility)
+        roleService.roleAbility = roleAbility
+        roleService.grantAbility = grantAbility
+        roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
+                                                       .roleName('admin')
+                                                       .roleCode("admin")
+                                                       .build()
+        ]
+        //roleAbility.createRole(_, _) >> true
+        roleAbility.createRole(_, _) >> {
+            spaceId, req ->
+                log.info("spaceId=$spaceId,req=$req")
+                true
+        }
+        grantAbility.grantPermissionsToRole(_) >> {
+            req ->
+                log.info("grant===>{}",req)
+                true
+        }
+
+        def spaceId = 1000
+        when:
+        def createResult = roleService.createRole(spaceId, RoleCreateReqDTO.builder()
+                .roleCode(RoleCodeGenerator.generate(RoleTypeEnum.admin,"1000"))
+                .roleName('admin1000')
+                .build())
+        then:
+        thrown(IllegalArgumentException)
+    }
 }
