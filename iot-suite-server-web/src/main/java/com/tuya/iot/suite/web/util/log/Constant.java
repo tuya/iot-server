88fd5c19500a1d564fa7c4702f4a0cd35c52d675
zhoujiaping <421211679@qq.com>
2021-06-04 16:37:01 +0800

    feat: add log xml

---
+++ iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/util/log/Constant.java
@@ -0,0 +1,10 @@
+package com.tuya.iot.suite.web.util.log;
+
+/**
+ * @author benguan.zhou@tuya.com
+ * @description
+ * @date 2021/06/04
+ */
+public interface Constant {
+    String TRACE_ID_KEY = "tid";
+}
