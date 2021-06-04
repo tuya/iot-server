b60ef2371ef82953a62b1731384807db398e510b
zhoujiaping <421211679@qq.com>
2021-06-04 14:58:28 +0800

    fix: 修复shiro过滤器链的bug（匹配上anon之后依然去匹配后面的path）。

--- iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/config/shiro/LoginFilter.java
+++ iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/config/shiro/LoginFilter.java
@@ -10,9 +10,8 @@ import com.tuya.iot.suite.web.util.SessionContext;
 import com.tuya.iot.suite.web.util.WebUtils;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.AccessControlFilter;
-
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -28,13 +27,19 @@ public class LoginFilter extends AccessControlFilter {
     @Setter
     private I18nMessage i18nMessage;
 
+    /**如果满足
+     * 1 当前的subject是被认证过的。
+     * 2 当前请求为登陆请求
+     * 只要满足2个条件的一个即可允许操作
+     */
     @Override
-    protected boolean isAccessAllowed(ServletRequest req, ServletResponse resp, Object arg2) throws Exception {
+    protected boolean isAccessAllowed(ServletRequest req, ServletResponse resp, Object mappedValue) {
         // 已登录或者是登录请求则允许访问
         HttpServletRequest request = (HttpServletRequest) req;
         if(log.isDebugEnabled()){
             log.debug(request.getRequestURI());
         }
+        //会话中有token，说明已经被认证过
         HttpSession session = request.getSession(false);
         if (session != null) {
             UserToken token = SessionContext.getUserToken();
@@ -44,19 +49,19 @@ public class LoginFilter extends AccessControlFilter {
                 return true;
             }
         }
-        if (SecurityUtils.getSubject().getPrincipal() != null) {
-            return true;
-        }
+        // 为登陆请求
         if(isLoginRequest(req, resp)){
             //用于登录成功后跳转到登录前想访问的页面
             this.saveRequest(req);
             return true;
         }
-        return false;
+        // 否则，进行认证
+        Subject subject = getSubject(req, resp);
+        return subject.isAuthenticated();
     }
 
     @Override
-    protected boolean onAccessDenied(ServletRequest req, ServletResponse resp) throws Exception {
+    protected boolean onAccessDenied(ServletRequest req, ServletResponse resp) {
         HttpServletResponse response = (HttpServletResponse) resp;
         String body = JSON.toJSONString(Response.buildFailure(ErrorCode.NO_LOGIN.getCode(),
                 i18nMessage.getMessage(ErrorCode.NO_LOGIN.getCode(), ErrorCode.NO_LOGIN.getMsg())));
