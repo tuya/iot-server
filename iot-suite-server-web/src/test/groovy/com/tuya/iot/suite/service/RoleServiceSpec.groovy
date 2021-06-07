8a409672804f9872f001c487ee0c9a17e82a7609
zhoujiaping <421211679@qq.com>
2021-06-07 10:24:27 +0800

    reactor: 重构测试用例（角色功能）

--- iot-suite-server-web/src/test/groovy/com/tuya/iot/suite/service/RoleServiceSpec.groovy
+++ iot-suite-server-web/src/test/groovy/com/tuya/iot/suite/service/RoleServiceSpec.groovy
@@ -9,11 +9,10 @@ import com.tuya.iot.suite.ability.idaas.model.IdaasPermission
 import com.tuya.iot.suite.ability.idaas.model.IdaasRole
 import com.tuya.iot.suite.ability.idaas.model.IdaasUser
 import com.tuya.iot.suite.ability.idaas.model.PermissionQueryByRolesRespItem
-import com.tuya.iot.suite.core.exception.ServiceLogicException
 import com.tuya.iot.suite.service.dto.RoleCreateReqDTO
 import com.tuya.iot.suite.service.idaas.RoleService
-import com.tuya.iot.suite.service.model.RoleCodeGenerator
-import com.tuya.iot.suite.service.model.RoleTypeEnum
+import com.tuya.iot.suite.test.Success
+import com.tuya.iot.suite.web.config.ProjectProperties
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -26,17 +25,19 @@ import org.springframework.beans.factory.annotation.Autowired
 class RoleServiceSpec extends BaseSpec {
 
     @Autowired
+    ProjectProperties projectProperties
+    @Autowired
     RoleService roleService
 
-    void "测试创建角色-创建成功"() {
+    void "测试创建角色"() {
         given:
         def roleAbility = Mock(RoleAbility)
         def grantAbility = Mock(GrantAbility)
         roleService.roleAbility = roleAbility
         roleService.grantAbility = grantAbility
         roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
-                                                       .roleName('admin')
-                                                       .roleCode("admin")
+                                                       .roleName(operatorRoleName)
+                                                       .roleCode(operatorRoleCode)
                                                        .build()
         ]
         //roleAbility.createRole(_, _) >> true
@@ -50,86 +51,26 @@ class RoleServiceSpec extends BaseSpec {
                 log.info("grant===>{}", req)
                 true
         }
-
         def spaceId = 1000
         when:
+        log.info("测试场景：{}",label)
         def createResult = roleService.createRole(spaceId, RoleCreateReqDTO.builder()
-                .roleCode(RoleCodeGenerator.generate(RoleTypeEnum.manage, "1000"))
-                .roleName('manage1000')
+                .roleCode(roleCode)
+                .roleName(roleName)
                 .build())
+        assert createResult
+        throw new Success()
         then:
-        createResult
+        thrown(expectedException)
+
+        where:
+        label|operatorRoleCode|operatorRoleName|roleCode||roleName||expectedException
+        '创建成功'|'admin'|'admin'|'manage-1000'|'manage1000'|| Success
+        '没有权限'|'normal'|'normal-1000'|'manage-1000'|'manage1000'||RuntimeException
+        '不能创建管理员'|'admin'|'admin'|'admin-1000'|'admin1000'||RuntimeException
     }
 
-
-    void "测试创建角色-没有权限"() {
-        given:
-        def roleAbility = Mock(RoleAbility)
-        def grantAbility = Mock(GrantAbility)
-        roleService.roleAbility = roleAbility
-        roleService.grantAbility = grantAbility
-        roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
-                                                       .roleName('normal')
-                                                       .roleCode("normal-1000")
-                                                       .build()
-        ]
-        //roleAbility.createRole(_, _) >> true
-        roleAbility.createRole(_, _) >> {
-            spaceId, req ->
-                log.info("spaceId=$spaceId,req=$req")
-                true
-        }
-        grantAbility.grantPermissionsToRole(_) >> {
-            req ->
-                log.info("grant===>{}", req)
-                true
-        }
-
-        def spaceId = 1000
-        when:
-        def createResult = roleService.createRole(spaceId, RoleCreateReqDTO.builder()
-                .roleCode(RoleCodeGenerator.generate(RoleTypeEnum.manage, "1000"))
-                .roleName('manage1000')
-                .build())
-        then:
-        thrown(ServiceLogicException)
-    }
-
-    void "测试创建角色-不能创建系统管理员"() {
-        given:
-        def roleAbility = Mock(RoleAbility)
-        def grantAbility = Mock(GrantAbility)
-        roleService.roleAbility = roleAbility
-        roleService.grantAbility = grantAbility
-        roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
-                                                       .roleName('admin')
-                                                       .roleCode("admin")
-                                                       .build()
-        ]
-        //roleAbility.createRole(_, _) >> true
-        roleAbility.createRole(_, _) >> {
-            spaceId, req ->
-                log.info("spaceId=$spaceId,req=$req")
-                true
-        }
-        grantAbility.grantPermissionsToRole(_) >> {
-            req ->
-                log.info("grant===>{}", req)
-                true
-        }
-
-        def spaceId = 1000
-        when:
-        def createResult = roleService.createRole(spaceId, RoleCreateReqDTO.builder()
-                .roleCode(RoleCodeGenerator.generate(RoleTypeEnum.admin, "1000"))
-                .roleName('admin1000')
-                .build())
-        then:
-        thrown(IllegalArgumentException)
-    }
-
-
-    void "测试删除角色-不能删除系统管理员"() {
+    void "测试删除角色"() {
         given:
         def roleAbility = Mock(RoleAbility)
         def grantAbility = Mock(GrantAbility)
@@ -138,13 +79,13 @@ class RoleServiceSpec extends BaseSpec {
         roleService.grantAbility = grantAbility
         roleService.idaasUserAbility = idaasUserAbility
         roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
-                                                       .roleName('admin')
-                                                       .roleCode("admin")
+                                                       .roleName(operatorRoleName)
+                                                       .roleCode(operatorRoleCode)
                                                        .build()
         ]
         roleAbility.deleteRole(_, _) >> {
-            spaceId, roleCode ->
-                log.info("spaceId=$spaceId,roleCode=$roleCode")
+            spaceId, roleCode0 ->
+                log.info("spaceId=$spaceId,roleCode=$roleCode0")
                 true
         }
         grantAbility.grantPermissionsToRole(_) >> {
@@ -155,131 +96,33 @@ class RoleServiceSpec extends BaseSpec {
         idaasUserAbility.queryUserPage(_, _) >> {
             spaceId, req ->
                 log.info("spaceId=$spaceId,req=$req")
-                IdaasPageResult.builder().build()
+                pageResult
         }
-
         def spaceId = 1000
+
         when:
-        def result = roleService.deleteRole(spaceId, "u123456", "admin")
+        log.info("测试场景：{}",label)
+        def result = roleService.deleteRole(spaceId, operatorUid, roleCode)
+        assert result
+        throw new Success()
+
         then:
-        thrown(IllegalArgumentException)
-    }
-
-    void "测试删除角色-存在关联的用户"() {
-        given:
-        def roleAbility = Mock(RoleAbility)
-        def grantAbility = Mock(GrantAbility)
-        def idaasUserAbility = Mock(IdaasUserAbility)
-        roleService.roleAbility = roleAbility
-        roleService.grantAbility = grantAbility
-        roleService.idaasUserAbility = idaasUserAbility
-        roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
-                                                       .roleName('admin')
-                                                       .roleCode("admin")
-                                                       .build()
-        ]
-        roleAbility.deleteRole(_, _) >> {
-            spaceId, roleCode ->
-                log.info("spaceId=$spaceId,roleCode=$roleCode")
-                true
-        }
-        grantAbility.grantPermissionsToRole(_) >> {
-            req ->
-                log.info("grant===>{}", req)
-                true
-        }
-        idaasUserAbility.queryUserPage(_, _) >> {
-            spaceId, req ->
-                log.info("spaceId=$spaceId,req=$req")
-                IdaasPageResult.builder()
-                        .totalCount(1)
-                        .results([IdaasUser.builder().username('monkey').build()])
-                        .build()
-        }
-
-        def spaceId = 1000
-        when:
-        def result = roleService.deleteRole(spaceId, "u123456", "manage-1000")
-        then:
-        thrown(ServiceLogicException)
-    }
-
-    void "测试删除角色-权限不足"() {
-        given:
-        def roleAbility = Mock(RoleAbility)
-        def grantAbility = Mock(GrantAbility)
-        def idaasUserAbility = Mock(IdaasUserAbility)
-        roleService.roleAbility = roleAbility
-        roleService.grantAbility = grantAbility
-        roleService.idaasUserAbility = idaasUserAbility
-        roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
-                                                       .roleName('normal1000')
-                                                       .roleCode("normal-1000")
-                                                       .build()
-        ]
-        roleAbility.deleteRole(_, _) >> {
-            spaceId, roleCode ->
-                log.info("spaceId=$spaceId,roleCode=$roleCode")
-                true
-        }
-        grantAbility.grantPermissionsToRole(_) >> {
-            req ->
-                log.info("grant===>{}", req)
-                true
-        }
-        idaasUserAbility.queryUserPage(_, _) >> {
-            spaceId, req ->
-                log.info("spaceId=$spaceId,req=$req")
-                IdaasPageResult.builder()
-                        .totalCount(0)
-                        .results([])
-                        .build()
-        }
-
-        def spaceId = 1000
-        when:
-        def result = roleService.deleteRole(spaceId, "u123456", "manage-1000")
-        then:
-        thrown(ServiceLogicException)
-    }
-
-    void "测试删除角色-成功"() {
-        given:
-        def roleAbility = Mock(RoleAbility)
-        def grantAbility = Mock(GrantAbility)
-        def idaasUserAbility = Mock(IdaasUserAbility)
-        roleService.roleAbility = roleAbility
-        roleService.grantAbility = grantAbility
-        roleService.idaasUserAbility = idaasUserAbility
-        roleAbility.queryRolesByUser(_, _) >> [IdaasRole.builder()
-                                                       .roleName('admin')
-                                                       .roleCode("admin")
-                                                       .build()
-        ]
-        roleAbility.deleteRole(_, _) >> {
-            spaceId, roleCode ->
-                log.info("spaceId=$spaceId,roleCode=$roleCode")
-                true
-        }
-        grantAbility.grantPermissionsToRole(_) >> {
-            req ->
-                log.info("grant===>{}", req)
-                true
-        }
-        idaasUserAbility.queryUserPage(_, _) >> {
-            spaceId, req ->
-                log.info("spaceId=$spaceId,req=$req")
-                IdaasPageResult.builder()
-                        .totalCount(0)
-                        .results([])
-                        .build()
-        }
-
-        def spaceId = 1000
-        when:
-        def result = roleService.deleteRole(spaceId, "u123456", "manage-1000")
-        then:
-        result
+        thrown(expectedException)
+        where:
+        label|operatorRoleCode|operatorRoleName|operatorUid|roleCode|pageResult||expectedException
+        '不能删除系统管理员'|'admin'|'admin'|'u123456'|'admin'|IdaasPageResult.builder().build()||RuntimeException
+        '存在关联的用户'|'admin'|'admin'|'u123456'|'admin'|IdaasPageResult.builder()
+                .totalCount(1)
+                .results([IdaasUser.builder().username('monkey').build()])
+                .build()||RuntimeException
+        '权限不足'|'normal-1000'|'normal1000'|'u123456'|'manage-1000'|IdaasPageResult.builder()
+                .totalCount(0)
+                .results([])
+                .build()||RuntimeException
+        '成功'|'admin'|'admin'|'u123456'|'manage-1000'|IdaasPageResult.builder()
+                .totalCount(0)
+                .results([])
+                .build()||Success
     }
 
     void "重新设置角色权限-成功"() {
