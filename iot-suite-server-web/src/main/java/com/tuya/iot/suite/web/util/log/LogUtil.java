a4447968980b9fa6ec03298b0105b9e7d3c3157a
zhoujiaping <421211679@qq.com>
2021-06-15 11:12:13 +0800

    reactor: optimize global exception handler

--- iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/util/log/LogUtil.java
+++ iot-suite-server-web/src/main/java/com/tuya/iot/suite/web/util/log/LogUtil.java
@@ -1,23 +1,19 @@
 package com.tuya.iot.suite.web.util.log;
 
+import com.google.common.base.Throwables;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 
 /**
- * 对于业务异常，我们一般不打印异常栈。
- * 但是没有异常栈，我们如何定位消息从哪里产生的呢？
+ * @description 对于业务异常，我们一般不打印异常栈。但是没有异常栈，我们如何定位消息从哪里产生的呢？
  * @author benguan.zhou@tuya.com
- * @description
  * @date 2021/06/04
  */
 @Slf4j
 public abstract class LogUtil {
-    public static void info(Logger logger,Exception e,String msg,Object... args){
+    public static void info(Logger logger,Throwable e,String msg,Object... args){
         try{
-            Throwable t = e;
-            while(t.getCause()!=null){
-                t = t.getCause();
-            }
+            Throwable t = Throwables.getRootCause(e);
             StackTraceElement st = t.getStackTrace()[0];
             String at = " at ";
             at += st.getClassName() + "#" + st.getMethodName() + ":" + st.getLineNumber();
